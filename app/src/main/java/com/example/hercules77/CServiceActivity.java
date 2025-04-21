d8931b2204b9edda49fbb91bf073d00048647d8f
stargazer <stargazer>
2025-04-21 13:24:21 +0700

    Fitur CS done

--- app/src/main/java/com/example/hercules77/CServiceActivity.java
+++ app/src/main/java/com/example/hercules77/CServiceActivity.java
@@ -20,21 +20,22 @@ public class CServiceActivity extends AppCompatActivity {
         btn_website = findViewById(R.id.btn_website);
         btn_email = findViewById(R.id.btn_email);
 
-        //Intent untuk website
+        //Intent ke website
         btn_website.setOnClickListener(v -> {
             Intent websiteIntent = new Intent(Intent.ACTION_VIEW);
-            websiteIntent.setData(Uri.parse("https://s.id/bersamastopjudol"));
+//            websiteIntent.setData(Uri.parse("https://s.id/bersamastopjudol"));
+            websiteIntent.setData(Uri.parse("https://youtu.be/dQw4w9WgXcQ?si=Eck1vZ1JhfWItKIQ"));
             startActivity(websiteIntent);
         });
 
-        //Intent untuk whatsapp
+        //Intent ke whatsapp
         btn_whatsapp.setOnClickListener(v -> {
             Intent messageIntent = new Intent(Intent.ACTION_VIEW);
             messageIntent.setData(Uri.parse("https://api.whatsapp.com/send/?phone=%2B6281110015080&text&type=phone_number&app_absent=0"));
             startActivity(messageIntent);
         });
 
-        // Intent untuk email
+        // Intent ke email
         btn_email.setOnClickListener(v -> {
             Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
             emailIntent.setData(Uri.parse("mailto:antijudol77@gmail.com"));
