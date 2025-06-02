24ba8d13c4eba7213f078f683dda7bc5d5b08eac
Akhtar Hafiz P <akhfizpuli@gmail.com>
2025-06-02 17:56:36 +0700

    added event logic (missing RecyclerView)

--- app/src/main/java/com/example/hercules77/EventActivity.java
+++ app/src/main/java/com/example/hercules77/EventActivity.java
@@ -1,24 +1,27 @@
 package com.example.hercules77;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.hercules77.databinding.ActivityEventBinding;
 
 public class EventActivity extends AppCompatActivity {
 
+    private ActivityEventBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_event);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityEventBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnAddEvent.setOnClickListener(v -> {
+            Intent intent = new Intent(this, EventFormActivity.class);
+            startActivity(intent);
         });
+
     }
 }
